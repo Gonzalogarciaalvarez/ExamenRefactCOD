beea5616da13e36de5201c0cba3ca2f7332037fb
ggarciaalvarez <ggarciaalvarez@atanasoff11.danielcastelao.org>
2015-12-10 09:22:25 +0100

    Insertarmetodo

--- src/examenrefactcod/ExamenRefactCOD.java
+++ src/examenrefactcod/ExamenRefactCOD.java
@@ -19,7 +19,7 @@ public class ExamenRefactCOD {
         int gid=3;
         int gidn=0;
         if(gid<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+        Pedirparametro();
         for(int i = 1; i <= 99999; i++ )
         {
             int aux = i;
@@ -34,34 +34,42 @@ public class ExamenRefactCOD {
         gidn=contador;
             
             
-            if(gidn==gid){
-                   if (i < 4) p = true;
+            Condicion(gidn, gid, i);
+        }
+        }
+
+    public static void Condicion(int gidn, int gid, int i) {
+        if(gidn==gid){
+            if (i < 4) p = true;
             else
             {
-            if (i % 2 == 0) p = false; 
+                if (i % 2 == 0) p = false; 
                 else
                 {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int k = (i - 1) / 2; 
-                    if  (k % 2 == 0) k--;  
-             
+                    int contador1 = 0;
+                    int i1 = 1;
+                    int k = (i - 1) / 2;
+                    if  (k % 2 == 0) k--;
+                    
                     while(i1 <= k)
                     {
                         if (i % i1 == 0) contador1++;
-                            i1 += 2;
-                            if (contador1 == 2) i1 = k + 1;
+                        i1 += 2;
+                        if (contador1 == 2) i1 = k + 1;
                     }
- 
-        if (contador1 == 1) p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
+                    
+                    if (contador1 == 1) p = true;
+                }    
             }
+            
+            if (p == true)
+                System.out.println(i);
         }
-        }
+    }
+
+    public static void Pedirparametro() {
+        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+    }
  
        
  
